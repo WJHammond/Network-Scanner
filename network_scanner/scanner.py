0b2b44497c558fed592423121cacc317eb958543
WJHammond <wjhammond@umass.edu>
2025-01-20 17:29:08 -0500

    <Ip-Range-Develoment><#2>:<Added the function and chenged test>

--- network_scanner/scanner.py
+++ network_scanner/scanner.py
@@ -9,7 +9,9 @@ def ping_ip(ip):
     """Ping an IP address to check if it's online."""
 
     # This checks which operating system you are currently using
-    param = "-n" if platform.system().lower() == "windows" else "-c"
+    if platform.system().lower() == "windows" :
+        param = "-n"  
+    else : "-c"
     command = ["ping", param, "1", ip]
     
     # This checks if the ip is online or offline
@@ -21,9 +23,19 @@ def ping_ip(ip):
     except subprocess.CalledProcessError:
         print(f"[-] {ip} is offline")
         return False
+def scan_ip_range(ipStart, ipEnd):
+    """Scan a range of IP addresses."""
+    start = int(ipStart.split('.')[-1])
+    end = int(ipEnd.split('.')[-1])
+    base_ip = '.'.join(ipStart.split('.')[:-1])  # Get the base (e.g., 192.168.1)
 
-# This tests the ping function
+    for i in range(start, end + 1):
+        ip = f"{base_ip}.{i}"
+        ping_ip(ip)
+
+
+# Test
 if __name__ == "__main__":
-    ip_to_test = "8.8.8.8"  # Google's public DNS
-    ping_ip(ip_to_test)
-
+    start_ip = "8.8.8.1"
+    end_ip = "8.8.8.10"
+    scan_ip_range(start_ip, end_ip)
